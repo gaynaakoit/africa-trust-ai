6e24b2c8a8c4c7763dff726b0e8d71715d68a576
Abdoulaye kane <abdoulaye.kane093@gmail.com>
2026-03-05 18:41:44 +0000

    Add features and shared component to 11 version

--- frontend/africa-trust-frontend/src/app/app.module.ts
+++ frontend/africa-trust-frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { AnalysisComponent } from './features/analysis/analysis.component';
+import { IdentityComponent } from './features/identity/identity.component';
+import { TrustComponent } from './features/trust/trust.component';
+import { VerifyComponent } from './features/verify/verify.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AnalysisComponent,
+    IdentityComponent,
+    TrustComponent,
+    VerifyComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
