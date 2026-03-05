39b0152dc8bf7f8472e330f5bfd290aefd04ef91
Abdoulaye kane <abdoulaye.kane093@gmail.com>
2026-03-05 18:43:25 +0000

    Add rooting

--- frontend/africa-trust-frontend/src/app/app-routing.module.ts
+++ frontend/africa-trust-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { AnalysisComponent } from './features/analysis/analysis.component';
+import { IdentityComponent } from './features/identity/identity.component';
+import { TrustComponent } from './features/trust/trust.component';
+import { VerifyComponent } from './features/verify/verify.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'analysis', component: AnalysisComponent },
+  { path: 'identity', component: IdentityComponent },
+  { path: 'trust', component: TrustComponent },
+  { path: 'verify', component: VerifyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
