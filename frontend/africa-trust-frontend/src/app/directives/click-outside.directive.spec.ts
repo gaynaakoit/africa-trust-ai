4b9167919a23be507506fcadcaf667f3e8fc5b30
Abdoulaye kane <abdoulaye.kane093@gmail.com>
2026-03-10 17:17:57 +0000

    Fix bug from ClickOutsideDirective

--- frontend/africa-trust-frontend/src/app/directives/click-outside.directive.spec.ts
+++ frontend/africa-trust-frontend/src/app/directives/click-outside.directive.spec.ts
@@ -1,8 +1,15 @@
 import { ClickOutsideDirective } from './click-outside.directive';
+import { ElementRef } from '@angular/core';
 
 describe('ClickOutsideDirective', () => {
+  let directive: ClickOutsideDirective;
+
+  beforeEach(() => {
+    const elementRef = new ElementRef(document.createElement('div'));
+    directive = new ClickOutsideDirective(elementRef);
+  });
+
   it('should create an instance', () => {
-    const directive = new ClickOutsideDirective();
     expect(directive).toBeTruthy();
   });
 });
